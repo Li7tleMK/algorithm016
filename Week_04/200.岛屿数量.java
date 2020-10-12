c677d3ab9e8c9ff9337726848430254619a879f9
Huang Boyi <huangboyi@howzhi.com>
2020-10-12 23:35:59 +0800

    add 127 200

---
+++ Week_04/200.岛屿数量.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.management.Query;
+
+/*
+ * @lc app=leetcode.cn id=200 lang=java
+ *
+ * [200] 岛屿数量
+ */
+
+// @lc code=start
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        int islandsCount = 0;
+
+        // BFS
+        for (int i = 0; i < nr; i++) {
+            for (int j = 0; j < nc; j++) {
+                if (grid[i][j] == '1') {
+                    islandsCount++;
+                    grid[i][j] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(i * nc + j);
+                    // 遍历到岛的四个方向，是否为1，如果是1放入队列
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            neighbors.add((row - 1) * nc + col);
+                            grid[row - 1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row + 1][col] == '1') {
+                            neighbors.add((row + 1) * nc + col);
+                            grid[row + 1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            neighbors.add(row * nc + col - 1);
+                            grid[row][col - 1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col + 1] == '1') {
+                            neighbors.add(row * nc + col + 1);
+                            grid[row][col + 1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return islandsCount;
+    }
+}
+// @lc code=end
