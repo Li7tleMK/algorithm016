27feec0e45598d02786dadeae76a7687db13d5d7
HuangBoyi <huangboyi@howzhi.com>
2020-10-25 20:53:18 +0800

    add week 05 homework

---
+++ Week_05/64.最小路径和.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=64 lang=java
+ *
+ * [64] 最小路径和
+ */
+
+// @lc code=start
+class Solution {
+    public int minPathSum(int[][] grid) {
+        // if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        // int m = grid.length;
+        // int n = grid[0].length;
+        // int[][] dp = new int[m][n];
+        // dp[0][0] = grid[0][0];
+        // for (int i = 1; i < m; i++) {
+        //     dp[i][0] = dp[i - 1][0] + grid[i][0];
+        // }
+        // for (int i = 1; i < n; i++) {
+        //     dp[0][i] = dp[0][i - 1] + grid[0][i];
+        // }
+        // for (int i = 1; i < m; i++) {
+        //     for (int j = 1; j < n; j++) {
+        //         dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        //     }
+        // }
+        // return dp[m - 1][n - 1];
+
+        // 空间优化版本：
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(i == 0 && j == 0) continue;
+                else if(i == 0)  grid[i][j] = grid[i][j - 1] + grid[i][j];
+                else if(j == 0)  grid[i][j] = grid[i - 1][j] + grid[i][j];
+                else grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+}
+// @lc code=end
+
