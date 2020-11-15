61bacf8c697ba99441640ac93764677c47e5b872
HuangBoyi <huangboyi@howzhi.com>
2020-11-15 22:52:13 +0800

    add week-09

---
+++ Week_09/387.字符串中的第一个唯一字符.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=387 lang=java
+ *
+ * [387] 字符串中的第一个唯一字符
+ */
+
+// @lc code=start
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            hashMap.put(s.charAt(i), hashMap.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hashMap.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
